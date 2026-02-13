8842a6f111482ddf3df6b06c09e099c87383fdb5
seo22hs <lsc584727@gmail.com>
2026-02-13 13:14:19 +0900

    add api project

---
+++ login.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+print("Content-type: text/html\r\n\r\n")
+print("<html>")
+print("<head><title>Login Result</title></head>")
+print("<body>")
+print("<h2>CGI 실행 성공</h2>")
+print("</body>")
+print("</html>")
+
